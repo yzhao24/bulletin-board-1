4def3b3bc17a9ee5d8f4743ee6044e58936e2ceb
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-05-11 03:45:16 +0000

    part 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,9 @@ class PostsController < ApplicationController
 
     if the_post.valid?
       the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
+      redirect_to("/boards/#{the_post.board_id}", { :notice => "Post created successfully." })
     else
-      redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/boards/#{the_post.board_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
